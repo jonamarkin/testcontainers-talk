409fc3fbb215a0ab0f38114496aaefc6a517b8b8
Jonathan Ato Markin <jona10711@gmail.com>
2025-07-06 23:13:33 +0200

    first commit

---
+++ src/main/java/com/markin/testcontainerstalk/TestcontainersTalkApplication.java
@@ -0,0 +1,13 @@
+package com.markin.testcontainerstalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcontainersTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestcontainersTalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/markin/testcontainerstalk/TestTestcontainersTalkApplication.java
@@ -0,0 +1,11 @@
+package com.markin.testcontainerstalk;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestTestcontainersTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(TestcontainersTalkApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
